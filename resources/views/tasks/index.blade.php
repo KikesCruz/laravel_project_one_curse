57f987ec0751281c1fab7623c2213b45d34f2e2e
Luis Enrique <50938247+KikesCruz@users.noreply.github.com>
2025-03-18 23:00:17 -0600

    se agrega formulario

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,7 +13,7 @@
                     <div class="p-4">
                         <a href="{{ route('tasks.create') }}">
                             <button type="button"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default</button>
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear tarea</button>
                         </a>
                     </div>
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
